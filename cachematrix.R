dbac264f820edc6a765ecca405b04cc00e7bcdbd
Dmitry Zinoviev <dzinoviev@gmail.com>
2015-03-10 21:16:47 -0400

    Initial revision

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The function creates a "smart matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = matrix ()) {
+  inverted <- NULL
+  set <- function (y)  {
+    x <<- y
+    inverted <<- NULL
+  }
+  get <- function () x
+  set.inverted <- function (inv) inverted <<- inv
+  get.inverted <- function () inverted
+  list (set = set, get = get, 
+        set.inverted = set.inverted, 
+        get.inverted = get.inverted)
 }
 
 
-## Write a short comment describing this function
+## The function computes an inverse of the "smart matrix"
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x, ...) {
+  inverted <- x$get.inverted ()
+  if (is.null (inverted)) { # The matrix has not been inverted recently!
+    mtx <- x$get ()
+    inverted <- solve (mtx, ...)
+    x$set.inverted (inverted)
+  }
+  inverted
 }
+
